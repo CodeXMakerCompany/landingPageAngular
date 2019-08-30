3adcbe59488f761db5f6dd92b60db2c4720339df
codexmaker <codexmaker12@gmail.com>
2019-08-29 23:34:19 -0500

    Auto-generated commit

---
+++ assets/libs/js/dialogs.js
@@ -0,0 +1,8 @@
+$("button").click(function() {
+  swal.fire("Success Message Title", "Well done, you pressed a button", "success")
+});
+
+
+function dialogo() {
+    swal.fire("Success Message Title", "Well done, you pressed a button", "success")
+}
