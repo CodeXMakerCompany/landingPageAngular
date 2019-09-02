3ce15503d59eb0934932e988b67dcc44c0d3f26b
codexmaker <codexmaker12@gmail.com>
2019-09-01 20:23:52 -0500

    updateLandingPage

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+declare const dialogo: any;
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class headerComponent {
+	onClick() {
+    infoPerfil();
+  	}
+
+  onClick2() {
+    pedido();
+  }
 
 }
--- src/assets/libs/js/dialogs.js
+++ src/assets/libs/js/dialogs.js
@@ -4,5 +4,44 @@ $("button").click(function() {
 
 
 function dialogo() {
-    swal.fire("Success Message Title", "Well done, you pressed a button", "success")
+    swal.fire("Proximamente", "Proximamente", "success")
+}
+
+function infoPerfil(){
+	 swal.fire("Trabajemos juntos!", 
+	 			"<h4>Te invito a dar un paseo por mi perfil.</h4><p>Si eres un desarrollador y te gustaria colaborar te dejo mi correo y mi cuenta de git para que me contactes.</p>",
+	 			"success")
+}
+
+function pedido(){
+	Swal.mixin({
+  input: 'text',
+  confirmButtonText: 'Next &rarr;',
+  showCancelButton: true,
+  progressSteps: ['1', '2', '3']
+}).queue([
+  {
+    title: 'Ingrese su correo',
+    text: 'Nos comunicaremos al correo que introduzca.'
+  },
+  {
+    title: 'Ingrese su nombre',
+    text: 'Queremos conocer a nuestro cliente!'
+  },
+  {
+    title: 'Especifique su proyecto',
+    text: 'Le ofreceremos la solución adecuada a su necesidad'
+  }
+]).then((result) => {
+  if (result.value) {
+    Swal.fire({
+      title: 'Perfecto!',
+      html:
+        '<h4>Nos comunicaremos con usted lo mas inmediato con una cotización, gracias por confiar en nostros!</h4>Sus datos: <pre><code>' +
+          JSON.stringify(result.value) +
+        '</code></pre>',
+      confirmButtonText: 'Enviar!'
+    })
+  }
+})
 }
